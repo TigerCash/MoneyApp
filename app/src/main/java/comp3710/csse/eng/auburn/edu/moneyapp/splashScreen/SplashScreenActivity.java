a44cee4b7694ba6dff19f3e25aa641a5938e805e
Dillon Beck <dillonbck@gmail.com>
2015-05-03 17:46:22 -0500

    Splash Screen and Icon

---
+++ app/src/main/java/comp3710/csse/eng/auburn/edu/moneyapp/splashScreen/SplashScreenActivity.java
@@ -0,0 +1,42 @@
+package comp3710.csse.eng.auburn.edu.moneyapp.splashScreen;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import comp3710.csse.eng.auburn.edu.moneyapp.R;
+import comp3710.csse.eng.auburn.edu.moneyapp.home.HomeActivity;
+
+public class SplashScreenActivity extends ActionBarActivity {
+
+	// Splash screen timer
+	private static int SPLASH_TIME_OUT = 3000;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash_screen);
+
+		new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+			@Override
+			public void run() {
+				// This method will be executed once the timer is over
+				// Start your app main activity
+				Intent i = new Intent(SplashScreenActivity.this, HomeActivity.class);
+				startActivity(i);
+
+				// close this activity
+				finish();
+			}
+		}, SPLASH_TIME_OUT);
+	}
+}
